234a1895ac1134003dc0d90e97697ce72ceefd36
Dongik Lee <dongik3475@gmail.com>
2019-01-04 18:03:55 +0900

    v2.0

--- app.py
+++ app.py
@@ -1,36 +1,81 @@
 from flask import Flask, request, jsonify, session, Session, render_template
+from flask_cors import CORS, cross_origin
 import pymongo
 from datetime import datetime
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
+CORS(app)
+cors = CORS(app, resources={
+  r"/v1/*": {"origin": "*"},
+  r"/api/*": {"origin": "*"},
+})
+
+# app.config['CORS_HEADERS'] = 'Content-Type'
 # app.config['JSON_AS_ASCII'] = False
+
+
 db = pymongo.MongoClient()
 rollbook_db = db.artistpizza.rollbooks
 members_db = db.artistpizza.members
+classes_db = db.artistpizza.classes
 
 def get_today_code():
     date = datetime.today()
     return int(date.strftime('%y%m%d'))
 
+
+@app.route('/api/weekdata', methods=['GET', 'PUT'])
+# @cross_origin(supports_credentials=True)
+# @cross_origin(origins=['http://0.0.0.0:5000/'])
+def route_week_data():
+    if request.method == 'GET':
+        week_id = datetime.today().strftime('%y년%W주차')
+        week_class = classes_db.find_one({"_id":week_id})
+        return jsonify(week_class)
+    elif request.method == 'PUT':
+        # print(request.json)
+        week_data = request.json
+        # print(week_data)
+        classes_db.save(week_data)
+        # return "good"
+        return jsonify(dict(message="good"))
+
+
+@app.route('/class')
+def route_class_view():
+    query = request.json
+    week_id = query['week_id']
+    week_class = classes_db.find_one({"_id":week_id})
+    rollbook = week_class[query['day']][query['time']][query['course']]
+    rollbook['title'] = "{0} {1} {2} {3}".format(week_id, query['day'], query['time'], query['course'])
+    return render_template("rollbook.html", rollbook=rollbook)
+
 @app.route('/rollbook')
 def route_rollbook_view():
+    day = request.args.get('day')
+    time = request.args.get('time')
+    course = request.args.get('course')
+    name = request.args.get('name')
     today = get_today_code()
-    today = 190103
-    rollbook = rollbook_db.find_one({"_id": today})
+    # today = 190103
+    rollbook = rollbook_db.find_one({"day": day, "time": time, "course": course, "name": name})
     del rollbook['_id']
-    title = "{} 출석부".format(today)
+    title = "{} 출석부".format(name)
     return render_template("rollbook.html", title=title, rollbook=rollbook)
 
 @app.route('/attend', methods=['POST'])
 def route_attend_api():
     print(request.json)
     attend_request = request.json
-    date = attend_request['date']
+    current_week = ""
     member_id = int(attend_request['member_id'])
     rollbook = rollbook_db.find_one({"_id": date})
+    class_board = classes_db.find_one({"_id": current_week})
     member = members_db.find_one({"_id": member_id})
+    course = member['course']
     if member is None:
         return jsonify(dict(message="wrong member id"))
     member_name = member['name']
+    rollbook = class_board[course['day']][course['time']][course['course']]
     if member_name in rollbook:
         print("check {}".format(rollbook[member_name]))
         if rollbook[member_name]['check'] == False:
@@ -45,7 +90,7 @@ def route_attend_api():
         return jsonify(dict(message="not booked"))
 
 # api
-@app.route('/rollbook/<date>', methods=['POST', 'GET', 'PUT'])
+@app.route('/rollbook', methods=['POST', 'GET', 'PUT'])
 def route_rollbook_api(date):
     if request.method == 'POST':
         rollbook = request.json
@@ -55,6 +100,10 @@ def route_rollbook_api(date):
     elif request.method == 'PUT':
         rollbook = request.json
         return jsonify(rollbook_db.save(rollbook))
+
+# @app.route('/weekclass', methods=['GET'])
+# def route_weekclass():
+#     return jsonify(rollbook_db.find({""}))
 
 
 @app.route('/members', methods=['POST', 'GET', 'PUT'])
--- init.py
+++ init.py
@@ -1,141 +1,80 @@
 import pymongo
+from datetime import datetime, timedelta
 
 db = pymongo.MongoClient()
 members_db = db.artistpizza.members
-courses_db = db.artistpizza.courses
+classes_db = db.artistpizza.classes
 
-목 = 4
-금 = 5
-토 = 6
+classes = {}
+classes['목요일'] = {}
+classes['목요일']['12-2시'] = {'수채화': {}, '유화': {}}
+classes['목요일']['2-4시'] = {'수채화': {}, '유화': {}}
+classes['목요일']['6-8시'] = {'수채화': {}, '유화': {}}
+classes['목요일']['8-10시'] = {'소묘드로잉': {}, '유화': {}}
+classes['금요일'] = {}
+classes['금요일']['12-2시'] = {'수채화': {}, '유화': {}}
+classes['금요일']['2-4시'] = {'수채화': {}, '유화': {}}
+classes['금요일']['6-8시'] = {'수채화': {}, '유화': {}}
+classes['금요일']['8-10시'] = {'소묘드로잉': {}, '유화': {}}
+classes['토요일'] = {}
+classes['토요일']['10-2시'] = {'master': {}}
+classes['토요일']['12-2시'] = {'수채화': {}, '유화': {}}
+classes['토요일']['2-4시'] = {'수채화': {}, '유화': {}}
+classes['토요일']['6-8'] = {"oneday": {}}
 
-n2h = {
-    4: "목",
-    5: "금",
-    6: "토"
-}
 
-courses = {
-    "목1": {
-        "name": "목1",
-        "weekday": 목,
-        "time": "목_1800"
-    },
-    "목2": {
-        "name": "목2",
-        "weekday": 목,
-        "time": "목_2000"
-    },
-    "금1": {
-        "name": "금1",
-        "weekday": 금,
-        "time": "금_1800"
-    },
-    "금2": {
-        "name": "금2",
-        "weekday": 금,
-        "time": "금_2000"
-    },
-    "토1": {
-        "name": "토1",
-        "weekday": 토,
-        "time": "토_1200"
-    },
-    "토2": {
-        "name": "토2",
-        "weekday": 토,
-        "time": "토_1400"
-    },
-}
 
-new_members = [
-    {
-        "_id": 93733471,
-        "name": "일동익",
-        "phone": "01093733471",
-        "count": 2,
-        "credit": 4500,
-        "course_name": "목1"
-    },
-    {
-        "_id": 93733472,
-        "name": "이동익",
-        "count": 4,
-        "phone": "01093733472",
-        "credit": 4500,
-        "course_name": "목2"
-    },
-    {
-        "_id": 93733473,
-        "name": "삼동익",
-        "phone": "01093733473",
-        "count": 5,
-        "credit": 4500,
-        "course_name": "금1"
-    },
-    {
-        "_id": 93733474,
-        "name": "사동익",
-        "phone": "01093733474",
-        "count": 6,
-        "credit": 4500,
-        "course_name": "금2"
-    },
-    {
-        "_id": 93733475,
-        "name": "오동익",
-        "phone": "01093733475",
-        "count": 7,
-        "credit": 4500,
-        "course_name": "토1"
-    },
-    {
-        "_id": 93733476,
-        "name": "육동익",
-        "phone": "01093733476",
-        "count": 7,
-        "credit": 4500,
-        "course_name": "토2"
-    },
-]
+
+
+# class number 22
+# member number = 220
+def generate_members():
+    members = []
+    count = 0
+    for first_name in ['김', '이', '박' , '서', '최', '인', '리', '나', '강']:
+        for middle_name in ['철', '동', '영', '양', '웅']:
+            for last_name in ['수', '희', '자', '진']:
+                name = first_name + middle_name + last_name
+                members.append({
+                    "name": name,
+                    "id": str(1000-count),
+                    "count": 3
+                })
+    return members
+
+
+new_members = generate_members()
+print("generate members number is {}".format(len(new_members)))
+
+# def insert_members(members):
+#     members_db.insert_many(members)
+#
+# insert_members()
+
+def fill_class_member(members, classes):
+    count = 0
+    while True:
+        for day in classes:
+            for time in classes[day]:
+                for course in classes[day][time]:
+                    # print("count is {}".format(count))
+                    member = members[count]
+                    # print(member)
+                    classes[day][time][course][member['name']] = False
+                    count += 1
+                    if len(members) == count:
+                        return
+
+
+fill_class_member(new_members, classes)
+
+
+
+def insert_week_class(week_class):
+    week_class['_id'] = datetime.today().strftime('%y년%W주차')
+    classes_db.insert_one(classes)
+    print("week class incerted")
+
+insert_week_class(classes)
 
 # members_db.insert_many(new_members)
-
-rollbook_db = db.artistpizza.rollbooks
-
-from datetime import datetime, timedelta
-
-rollbooks = {}
-
-
-
-def book2(member, course):
-    for i in range(member['count']):
-        print("count {}".format(i))
-        # calculate book date
-        gap = course['weekday'] - datetime.today().weekday() + 7 * (i + 1) - 1
-        # if gap < 1:
-        #     gap += 7
-        date = datetime.today() + timedelta(days=gap)
-        bookdate = int(date.strftime('%y%m%d'))
-        # bookdate = "{0}_{1}".format(bt, course['time'])
-        print("booktime is {}".format(bookdate))
-
-        rollbook = rollbook_db.find_one({"_id": bookdate})
-        if rollbook is None:
-            rollbook_db.insert_one({"_id": bookdate})
-            rollbook = rollbook_db.find_one({"_id": bookdate})
-
-        rollbook[member['name']] = dict(course=member['course_name'], check=False)
-
-        rollbook_db.save(rollbook)
-
-def book(member):
-    course = courses[member['course_name']]
-    book2(member, course)
-
-def book_all():
-    for member in members_db.find():
-        print("booking {}".format(member))
-        book(member)
-
-book_all()
